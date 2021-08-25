836fd50bcb3630b1b4f9644f0611fd4bae021dbd
irison <0xirison@gmail.com>
2021-08-25 19:27:50 +0300

    updating some minor things

--- wifi-passwords-revealer.py
+++ wifi-passwords-revealer.py
@@ -6,7 +6,8 @@ import platform
 
 if "windows" in platform.system().lower():
     try:
-        print("""
+        print(
+            """
  ________ __   ___ __ 
 |  |  |  |__|.'  _|__|
 |  |  |  |  ||   _|  |
@@ -21,40 +22,59 @@ if "windows" in platform.system().lower():
 |   __ \.-----.--.--.-----.---.-.|  |.-----.----.
 |      <|  -__|  |  |  -__|  _  ||  ||  -__|   _|
 |___|__||_____|\___/|_____|___._||__||_____|__|  
-                                                                                                 
+                                                                                             
 ==========================
 Wifi Passwords Revealer v1.0
 Author: irison
 GitHub: https://github.com/0xirison
 *._.* __ _ ._ 
 |[  |_) (_)[ )
-==========================  """)
+========================== 
+ """
+        )
         wifi_list = []
         wifi_dict = {}
-        show_wifi_command = subprocess.run('netsh wlan show profile ', shell=False, stdout=subprocess.PIPE)
+        show_wifi_command = subprocess.run(
+            "netsh wlan show profile ", shell=False, stdout=subprocess.PIPE
+        )
 
-        for line in show_wifi_command.stdout.decode('latin-1').splitlines():
-            if "All" in line.replace(" ",""):
+        for line in show_wifi_command.stdout.decode("latin-1").splitlines():
+            if "All" in line.replace(" ", ""):
                 delim = ": "
-                wifi_list.append(line[line.index(delim) + len(delim):])
+                wifi_list.append(line[line.index(delim) + len(delim) :])
 
         for wifi_details in wifi_list:
-            show_wifi_password = subprocess.run('netsh wlan show profile '+ wifi_details + " key=clear", shell=False, stdout=subprocess.PIPE)
-            for line in show_wifi_password.stdout.decode('latin-1').splitlines():
+            show_wifi_password = subprocess.run(
+                "netsh wlan show profile " + wifi_details + " key=clear",
+                shell=False,
+                stdout=subprocess.PIPE,
+            )
+            for line in show_wifi_password.stdout.decode("latin-1").splitlines():
                 if "Key Content" in line.replace("\r\n", "").strip():
                     delim = ": "
-                    wifi_dict[wifi_details] = line[line.index(delim) + len(delim):]
+                    wifi_dict[wifi_details] = line[line.index(delim) + len(delim) :]
                 else:
                     continue
-        file= open(os.path.normpath(os.path.expanduser("~/Desktop")).replace("\\",'\\\\')+ "\\Wifi_Passwords-" +str(datetime.datetime.now()).replace(":", "-").replace(" ","-").split(".")[0] +".txt","w+")
+        file = open(
+            os.path.normpath(os.path.expanduser("~/Desktop")).replace("\\", "\\\\")
+            + "\\Wifi_Passwords-"
+            + str(datetime.datetime.now())
+            .replace(":", "-")
+            .replace(" ", "-")
+            .split(".")[0]
+            + ".txt",
+            "w",
+        )
         for key, values in wifi_dict.items():
-            file.write(key +": " + values+"\r\n")
+            file.write(key + ": " + values + "\r\n")
         file.close()
-        print("[+] All your previous logged in Wifi SSIDs and passwords are saved on your Desktop!")
+        print(
+            "[+] All your previous logged-in Wifi SSIDs and passwords are saved on your Desktop!"
+        )
     except:
-        print("[-] Sorry, this windows system version does not support the used utility commands")
+        print(
+            "[-] Sorry, this windows system version does not support the used utility commands"
+        )
 
 else:
     print("[-] Sorry, this tool works only on Windows systems")
-
-
